ee093d9cf5aa2118e6c90d4572bbd5aca12a5fd1
rlatosky <rlatosky@posteo.net>
2024-03-14 07:46:49 -0400

    modify joints to make them more accurate. Update the python script to do several tests between certain times

--- simulate-triangle-car.py
+++ simulate-triangle-car.py
@@ -13,16 +13,31 @@ with mujoco.viewer.launch_passive(m, d) as viewer:
   # Close the viewer automatically after 30 wall-seconds.
   start = time.time()
   # Set an initial velocity for a joint (e.g., assuming joint index 0)
-  initial_velocity = 15  # set your desired initial velocity
+  initial_velocity = 1  # set your desired initial velocity
   print(m.actuator_user)
   #m.jnt_dofadr = initial_velocity
   #m.jnt_dofadr = -initial_velocity
-  while viewer.is_running() and time.time() - start < 30:
-    # print('velocities', d.qvel)
+  while viewer.is_running() and time.time() - start < 120:
+    print('pos', d.qpos[0])
     step_start = time.time()
+    print("Current time:", time.time() - start)
+    if time.time() - start < 20:
+        d.ctrl[0] = -initial_velocity*d.time
+        d.ctrl[1] = initial_velocity*d.time
+        d.ctrl[2] = -initial_velocity*d.time
+    if 20 <= (time.time() - start) < 40:
+        d.ctrl[0] = initial_velocity*d.time
+        d.ctrl[1] = -initial_velocity*d.time
+        d.ctrl[2] = initial_velocity*d.time
+    if 40 <= (time.time() - start) < 60:
+        d.ctrl[0] = initial_velocity*d.time
+        d.ctrl[1] = initial_velocity*d.time
+        d.ctrl[2] = -initial_velocity*d.time
+    if 60 <= (time.time() - start) < 120:
+        d.ctrl[0] = -initial_velocity*d.time
+        d.ctrl[1] = initial_velocity*d.time
+        d.ctrl[2] = initial_velocity*d.time
 
-    d.ctrl[0] = initial_velocity*d.time
-    d.ctrl[2] = -initial_velocity*d.time
     # mj_step can be replaced with code that also evaluates
     # a policy and applies a control signal before stepping the physics.
     mujoco.mj_step(m, d)
